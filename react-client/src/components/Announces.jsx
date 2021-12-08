4d75899c81d059babaa695822ebbf8a0d878f543
naceurmossaab <naceurmossaab@gmail.com>
2021-12-08 13:31:43 +0100

    Home Component 1.0

---
+++ react-client/src/components/Announces.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Annouces extends Component {
+     constructor() {
+          super();
+     }
+
+     render() {
+          return (
+               <div>
+                    Annouces component
+               </div>
+          )
+     }
+}
---
+++ react-client/src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+     constructor() {
+          super();
+     }
+
+     render() {
+          return (
+               <div>
+                    Contact component
+               </div>
+          )
+     }
+}
