bf3860f601d0769229ace2f777a699109c981716
Firas Jaballah <jaballah.firas89@gmail.com>
2021-12-09 15:02:36 +0100

    unapdated feeds

--- react-client/src/components/Feed.jsx
+++ react-client/src/components/Feed.jsx
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
 
-export default class Feed extends Component {
-     constructor() {
-          super();
-     }
+import React, { useState, useEffect } from 'react';
+import Collapsible from "react-collapsible";
+import feed from "../../../dammyData/feedDAta.js";
 
-     render() {
-          return (
-               <div>
-                    Feed component
-               </div>
-          )
-     }
+export default function Feed  () {
+     const [feeds, setFeeds] = useState(feed);
+
+
+
+    return (
+      <div>
+        {feed.map((e, i) => (
+          <div key={i}>
+            <h3>{e.post.user}</h3>
+            <p>{e.post.text}</p>
+            <p>{e.post.createdAt}</p>
+            <input   defaultValue= {"reply to " +e.post.user}></input>
+               <button >Reply</button>
+            <Collapsible trigger="Replies" >
+              {e.replies.map((reply, index) => (
+                <div key={index}>
+                  <h3>{reply.user}</h3>
+                  <p>{reply.text}</p>
+                  <p>{reply.createdAt}</p>
+                </div>
+              ))}
+            </Collapsible>
+          </div>
+        ))}
+      </div>
+    );
+  
 }
