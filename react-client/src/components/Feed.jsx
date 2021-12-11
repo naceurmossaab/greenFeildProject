89916bff8678f279b1a8d7fc24c4dc26a79ccdeb
Firas Jaballah <jaballah.firas89@gmail.com>
2021-12-11 12:11:32 +0100

    feed

--- react-client/src/components/Feed.jsx
+++ react-client/src/components/Feed.jsx
@@ -2,28 +2,51 @@ import React, { useState, useEffect } from "react";
 import Collapsible from "react-collapsible";
 import feed from "../../../dammyData/feedDAta.js";
 import axios from "axios";
+import $ from 'jquery';
+
 
 export default function Feed() {
   const [feeds, setFeeds] = useState(feed);
   const [txt, setReply] = useState("");
+
   useEffect(() => {
     axios.get("/feeds").then((res) => {
       setFeeds(res.data);
     });
   });
+  function addPost(){
+    $(".Post").toggle()
+  }
+  function writeReply(){
+    $(".show").toggle()
+  }
+  
 
   return (
     <div>
+      <button onClick={()=>addPost()}>Create a post +</button>
+      <div className="Post" style={{display: "none"}}>
+          <input placeholder="region"
+            
+          ></input>
+          <textarea className="create-body-textarea" placeholder="your post here.."
+            
+          ></textarea>
+          <button>Post</button>
+          </div>
       {feeds.map((e, i) => (
         <div key={i}>
           <h3>{e.post.user}</h3>
           <p>{e.post.text}</p>
           <p>{e.post.createdAt}</p>
+          <button onClick={()=>writeReply()}>Comment</button>
+          <div className="show" style={{display: "none"}}>
           <input
             onChange={(event) => setReply(event.value)}
             defaultValue={"reply to " + e.post.user}
           ></input>
-          <button>Reply</button>
+          <button>Post the comment</button>
+          </div>
           <Collapsible trigger="Replies">
             {e.replies.map((reply, index) => (
               <div key={index}>
