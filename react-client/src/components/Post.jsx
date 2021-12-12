57ed5df833b93fb3ae9b0569054f41e647906e28
naceurmossaab <naceurmossaab@gmail.com>
2021-12-12 22:43:46 +0100

    post model

--- react-client/src/components/Post.jsx
+++ react-client/src/components/Post.jsx
@@ -30,20 +30,6 @@ const Post = (props) => {
 				{post.discription}
 			</div>
 		</div>
-		// <div className="postCard">
-		// 	<div className='postImg'>
-		// 		<img src={post.pics[0]} />
-		// 		<img src={post.pics[1]} />
-		// 		<img src={post.pics[2]} />
-		// 		<img src={post.pics[3]} />
-		// 	</div>
-		// 	<div className='postDetails'>
-		// 		<div>{post.address}</div>
-		// 		<div>{post.numberOfRooms}</div>
-		// 		<div>{post.price}</div>
-		// 	</div>
-		// 	<div className='postDiscription'>{post.discription}</div>
-		// </div>
 	);
 };
 
